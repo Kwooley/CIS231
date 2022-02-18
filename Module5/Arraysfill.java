e8457069c392a7de527a419bf8b0478fa837bda4
Kyu Lee <kwooler@gmail.com>
2022-02-17 21:56:05 -0800

    Module5

---
+++ Module5/Arraysfill.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Arraysfill {
+	public static void main(String[] args) {
+		final int N = 10;
+		int[] numbers = new int[N];
+
+		Arrays.fill(numbers, 10);
+
+		for (int v : numbers)
+			System.out.print(v + "\t");
+		System.out.println();
+	}
+
+}
